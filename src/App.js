c08d705ba37a23a6641de1aad55e272e7fde0b27
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-07-02 12:18:31 +0530

    prevent form data to be saved if description is empty

--- src/App.js
+++ src/App.js
@@ -28,6 +28,8 @@ function Form() {
   function handleSubmit(e) {
     e.preventDefault();
 
+    if (!description) return;
+
     const newItem = { description, quantity, packed: false, id: Date.now() };
     // console.log(newItem);
   }
