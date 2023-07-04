d52f35c3476dac589a4d7ff01a3c9c39fa3354db
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-07-04 11:31:29 +0530

    create logic to sort by packed status

--- src/App.js
+++ src/App.js
@@ -90,6 +90,11 @@ function PackagingList({ items, onDeleteItem, onToggleItem }) {
       .slice()
       .sort((a, b) => a.description.localeCompare(b.description));
 
+  if (sortBy === "packed")
+    sortByItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul>
