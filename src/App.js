5fdd91f22cdb4be616e906f6c5880078161e027f
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-07-04 11:20:35 +0530

    specify sort options

--- src/App.js
+++ src/App.js
@@ -91,6 +91,14 @@ function PackagingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
