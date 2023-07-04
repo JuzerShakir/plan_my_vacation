c3b66639597d9b4261dd55c5cff18107e3867218
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-07-04 10:40:00 +0530

    show stats

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -111,10 +111,25 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+  }
+
+  const numCount = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = (numPacked / numCount) * 100;
+
   return (
     <footer className="stats">
-      <em> 💼 You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        💼 You have {numCount} items on your list, and you already packed{" "}
+        {numPacked} ({percentage}%)
+      </em>
     </footer>
   );
 }
